5c677155c64cc9e825cc1c3bba1676def93712c8
AlexandrTurdiyev <alexandr.turdiyev@gmail.com>
2023-11-29 14:47:28 +0530

    feat(project articles): add optimization of pages of all existing project posts using lazy loading of images. Update images and use the picture tag;
    Wed Nov 29 2023 14:45:29 GMT+0530 (India Standard Time);

--- scripts/lazy-loading-images.js
+++ scripts/lazy-loading-images.js
@@ -1,44 +1,3 @@
-// const lazyImages = document.querySelectorAll('img[data-src], source[data-srcset]');
-//
-// const windowHeight = document.documentElement.clientHeight;
-//
-// let lazyImagesPositions = [];
-//
-// if (lazyImages.length>0) {
-//     lazyImages.forEach(img => {
-//         if (img.dataset.src || img.dataset.srcset) {
-//             lazyImagesPositions.push(img.getBoundingClientRect().top + scrollY);
-//             lazyScrollCheck();
-//         }
-//     })
-// }
-//
-// window.addEventListener("scroll", lazyScroll);
-//
-// function lazyScroll () {
-//     if (document.querySelectorAll('img[data-src], source[data-srcset]').length > 0) {
-//         lazyScrollCheck();
-//         console.log(scrollY);
-//     }
-// }
-//
-// function lazyScrollCheck () {
-//     let imgIndex = lazyImagesPositions.findIndex(
-//         item => scrollY > item - windowHeight
-//     );
-//     if (imgIndex >= 0) {
-//         if (lazyImages[imgIndex].dataset.src) {
-//             lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
-//             lazyImages[imgIndex].removeAttribute('data-src');
-//         } else if (lazyImages[imgIndex].dataset.srcset) {
-//             lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
-//             lazyImages[imgIndex].removeAttribute('data-srcset');
-//         }
-//         delete lazyImagesPositions[imgIndex];
-//     }
-// }
-/* ------------------------------------------------------------------------------------------ */
-
 const images = document.querySelectorAll('img');
 
 const options = {
@@ -49,7 +8,6 @@ const options = {
 
 function handleImg( myImg, observer) {
     myImg.forEach( myImgSingle => {
-        console.log(myImgSingle.intersectionRatio);
         if (myImgSingle.intersectionRatio > 0) {
             loadImage (myImgSingle.target);
         }
