80b44418b2f80da486cbdbbf2ecd90acb58a57b0
AlexandrTurdiyev <alexandr.turdiyev@gmail.com>
2024-01-03 18:53:21 +0530

    add: webpack plugins for styles and web-server autostart and other code settings;
    Wed Jan 03 2024 18:53:09 GMT+0530 (India Standard Time);

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.NODE_ENV || "development";
 
@@ -12,23 +13,47 @@ module.exports = {
     mode,
     target,
     devtool,
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        filename: 'index.[contenthash].js'
+        filename: '[name].[contenthash].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html')
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.html/i,
                 loader: 'html-loader'
-            }
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')]
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ],
+            },
         ]
     }
 }
