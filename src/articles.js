3fd060c5c00992951f9785d0f873448b34280550
AlexandrTurdiyev <alexandr.turdiyev@gmail.com>
2024-01-11 14:45:07 +0530

    add: lazy loading for images on projects and other pages;
    Thu Jan 11 2024 14:44:52 GMT+0530 (India Standard Time);

--- src/articles.js
+++ src/articles.js
@@ -5,4 +5,6 @@ import('./pages/projects/residential/zhk-rams-city-almaty-2023/zhk-rams-city-alm
 import('./pages/projects/residential/zhk-khan-tengri-almaty-2023/zhk-khan-tengri-almaty-2023.html');
 import('./pages/projects/commercial/cafe-vanilla-almaty-2023/cafe-vanilla-almaty-2023.html');
 import('./pages/projects/commercial/kofejnya-momento-almaty-2023/kofejnya-momento-almaty-2023.html');
+import('./modules/preloader/_preloader');
+import('./modules/content-load/content-load');
 import('./articles.scss')
