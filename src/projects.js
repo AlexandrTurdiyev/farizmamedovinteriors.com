3a379521030e5e6ba547e41ac955ee2e9f867b44
AlexandrTurdiyev <alexandr.turdiyev@gmail.com>
2024-01-12 11:49:20 +0530

    feat: update filter;
    Fri Jan 12 2024 11:49:07 GMT+0530 (India Standard Time);

--- src/projects.js
+++ src/projects.js
@@ -2,3 +2,4 @@ import('./projects.html');
 import('./projects.scss');
 import ('./modules/preloader/_preloader');
 import('./modules/content-load/content-load');
+import('./modules/filter-content/filter-content');
--- src/publications.js
+++ src/publications.js
@@ -2,3 +2,4 @@ import('./publications.html');
 import('./publications.scss');
 import './modules/preloader/_preloader';
 import('./modules/content-load/content-load');
+import('./modules/filter-content/filter-content');
