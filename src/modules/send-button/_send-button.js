cac3127198f03ea160bba78b6921910153cabdc3
AlexandrTurdiyev <alexandr.turdiyev@gmail.com>
2024-01-13 11:41:31 +0530

    feat: add form handler and button styles;
    Sat Jan 13 2024 11:41:19 GMT+0530 (India Standard Time);

--- src/modules/send-button/_send-button.js
+++ src/modules/send-button/_send-button.js
@@ -4,40 +4,76 @@ const buttonText = document.querySelector('.button__text');
 const buttonTextWrapper = document.querySelector('.button__text-wrapper');
 const doneIcon = document.querySelector('.button__done-icon-img');
 
-button.addEventListener('click', function (event) {
-    button.disabled = true;
-    buttonText.classList.add('opacity--zero');
-    button.classList.add('button--fix-size');
-    aeroPlane.classList.add('plane--rotate');
-    console.log('click button');
-    function sendFinish () {
-        button.classList.remove('button--fix-size');
-        aeroPlane.classList.remove('plane--rotate');
-        button.disabled = false;
-    }
-    setTimeout(buttonAddSendingText, 2000);
-    setTimeout(sendFinish, 5000);
-    setTimeout(buttonAddSendText, 5000);
-    setTimeout(hideDoneIcon, 4000);
+/* Form handler */
+const contactForm = document.querySelector('form');
+contactForm.addEventListener('submit', function (event){
+    event.preventDefault();
+    formSubmit();
+});
 
-    function buttonAddSendText () {
 
-        buttonText.classList.remove('opacity--zero');
-        setTimeout(changeText, 1000);
-        function changeText () {
-            buttonText.innerHTML = 'ОТПРАВИТЬ';
+/* - fields check */
+function isRequiredFieldsFilling () {
+    const requiredFields = document.querySelectorAll('input[required]');
+    let emptyRequiredFieldsCount = 0;
+    requiredFields.forEach((element) => {
+        element.value === '' ? emptyRequiredFieldsCount++ : false;
+    })
+    return emptyRequiredFieldsCount === 0;
+}
+/* - fields check [end] */
 
-        }
-    }
-
-    function buttonAddSendingText () {
-        doneIcon.classList.add('opacity--visible');
-        buttonText.innerHTML = 'ОТПРАВЛЕНО';
-        buttonTextWrapper.classList.add('opacity--visible');
-        buttonText.classList.remove('opacity--zero');
-    }
-
-    function hideDoneIcon () {
-        doneIcon.classList.remove('opacity--visible');
+/* - submit form */
+button.addEventListener('click', event => {
+    if (isRequiredFieldsFilling()) {
+        formSubmit();
+    } else {
+        buttonShaking();
     }
 })
+function formSubmit () {
+        button.disabled = true;
+        buttonText.classList.add('opacity--zero');
+        button.classList.add('button--fix-size');
+        aeroPlane.classList.add('plane--rotate');
+        console.log('click button');
+        setTimeout(buttonAddSendingText, 2000);
+        setTimeout(sendFinish, 5000);
+        setTimeout(buttonAddSendText, 5000);
+        setTimeout(hideDoneIcon, 4000);
+}
+function buttonAddSendingText () {
+    doneIcon.classList.add('opacity--visible');
+    buttonText.innerHTML = 'ОТПРАВЛЕНО';
+    buttonTextWrapper.classList.add('opacity--visible');
+    buttonText.classList.remove('opacity--zero');
+    button.classList.add('form--submit');
+}
+function sendFinish () {
+    button.classList.remove('button--fix-size');
+    aeroPlane.classList.remove('plane--rotate');
+    button.disabled = false;
+}
+function buttonAddSendText () {
+    buttonText.classList.remove('opacity--zero');
+    setTimeout(changeText, 1000);
+    function changeText () {
+        buttonText.innerHTML = 'ОТПРАВИТЬ';
+    }
+}
+function hideDoneIcon () {
+    doneIcon.classList.remove('opacity--visible');
+    button.classList.remove('form--submit');
+}
+function buttonShaking () {
+    button.classList.add('button--shaking');
+    setTimeout(buttonStopShaking, 200);
+
+    function buttonStopShaking () {
+        button.classList.remove('button--shaking')
+    }
+}
+
+/* - submit form [end] */
+/* Form handler [end] */
+
