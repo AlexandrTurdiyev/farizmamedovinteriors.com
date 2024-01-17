68e7904a3e8eec5cc4a9bc6a7184f89fbda7380b
AlexandrTurdiyev <alexandr.turdiyev@gmail.com>
2024-01-18 01:11:45 +0530

    add: scroll to top module and connet this module for all page (index.html - not);
    Thu Jan 18 2024 01:11:33 GMT+0530 (India Standard Time);

--- src/about-us.js
+++ src/about-us.js
@@ -2,3 +2,4 @@ import('./about-us.html')
 import('./about-us.scss')
 import('./modules/content-load/content-load');
 import ('./modules/preloader/_preloader');
+import('./modules/scroll-to-top/_scroll-to-top');
